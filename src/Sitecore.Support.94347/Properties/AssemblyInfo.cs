7b4e44d1d7a3d0637528af65019525fdd3c54421
DK\IEKA <ieka@sitecore.net>
2017-06-07 10:33:48 +0300

    Add buttom that invokes "New package from existing" functionality.

---
+++ src/Sitecore.Support.94347/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.94347")]
+[assembly: AssemblyProduct("Sitecore.Support.94347")]
+[assembly: ComVisible(false)]
